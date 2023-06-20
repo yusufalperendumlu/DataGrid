99490f7d4353b3df872568eacb5aea78a275dba7
Yusuf Alperen <yusufalperendumlu@gmail.com>
2023-06-20 22:44:12 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import "devextreme/dist/css/dx.light.css";
-import { useState } from "react";
 import "./index.css";
 
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
